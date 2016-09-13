a6b26c2a265559f7c38dfb900ff575aac7f90834
camicalca <camicalcagno12@gmail.com>
2016-09-13 19:15:04 -0300

    modifications of implementations BandDAO.java

--- src/main/java/ar/edu/unrc/exa/dc/dose2016/riocuartobandasderock/dao/BandDAO.java
+++ src/main/java/ar/edu/unrc/exa/dc/dose2016/riocuartobandasderock/dao/BandDAO.java
@@ -1,4 +1,5 @@
 package ar.edu.unrc.exa.dc.dose2016.riocuartobandasderock.dao;
+
 import java.util.List;
 
 import ar.edu.unrc.exa.dc.dose2016.riocuartobandasderock.model.Band;
@@ -11,11 +12,11 @@ public interface BandDAO {
 	   
 	   public Band getBand(int id);
 	   
-	   public void updateBand(Band band);
+	   public Boolean updateBand(Band band);
 	   
-	   public void deleteBand(Band band);
+	   public Boolean deleteBand(Band band);
 	   
-	   public void addBand(Band band);
+	   public Boolean addBand(Band band);
 	   
 	   
 	
