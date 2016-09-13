f9ecdda598322027f5dca9e3bc4a04df3aef2946
camicalca <camicalcagno12@gmail.com>
2016-09-13 19:04:58 -0300

    Implemented interface BandDAO.java

--- src/main/java/ar/edu/unrc/exa/dc/dose2016/riocuartobandasderock/dao/BandDAO.java
+++ src/main/java/ar/edu/unrc/exa/dc/dose2016/riocuartobandasderock/dao/BandDAO.java
@@ -1,5 +1,23 @@
 package ar.edu.unrc.exa.dc.dose2016.riocuartobandasderock.dao;
+import java.util.List;
+
+import ar.edu.unrc.exa.dc.dose2016.riocuartobandasderock.model.Band;
+
 
 public interface BandDAO {
-
+	
+	   
+	   public List<Band> getAllBands();
+	   
+	   public Band getBand(int id);
+	   
+	   public void updateBand(Band band);
+	   
+	   public void deleteBand(Band band);
+	   
+	   public void addBand(Band band);
+	   
+	   
+	
 }
+
