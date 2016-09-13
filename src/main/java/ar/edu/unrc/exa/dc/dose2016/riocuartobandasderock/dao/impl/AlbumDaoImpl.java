72f5a3ea8e88d4ad5333afba2092d4bdddb21ce0
dmatiasr <matu_dmr@hotmail.com>
2016-09-13 20:11:45 -0300

    AlbumDaoImpl.class is done

---
+++ src/main/java/ar/edu/unrc/exa/dc/dose2016/riocuartobandasderock/dao/impl/AlbumDaoImpl.java
@@ -0,0 +1,64 @@
+package ar.edu.unrc.exa.dc.dose2016.riocuartobandasderock.dao.impl;
+
+import java.util.List;
+
+import ar.edu.unrc.exa.dc.dose2016.riocuartobandasderock.model.Song;
+import ar.edu.unrc.exa.dc.dose2016.riocuartobandasderock.dao.AlbumDAO;
+import ar.edu.unrc.exa.dc.dose2016.riocuartobandasderock.model.Album;
+
+public class AlbumDaoImpl implements AlbumDAO{
+
+	public Album findById(int id){
+		return null;
+	}
+	
+	public List<Album> getAllAlbums(){
+		return null;
+	}	
+	
+	public List<Album> findByBandName(String bandName){
+		return null;
+	}
+	
+	public Album findByName(String name){
+		return null;
+	}
+	
+	public List<Album> findByYear(int year){
+		return null;
+	}
+	public List<Album> findByGenere(String genere){
+		return null;
+	}
+
+	public List<Album> findByRecordLabel(String recordLabel){
+		return null;
+	}
+	
+	public List<Album> findByProducer(String producer){
+		return null;
+	}
+	
+	public List<Album> findByDuration(int duration){
+		return null;
+	}
+	
+	public List<Album> findBySong(Song song){
+		return null;
+	}
+
+	public void createAlbum(Album album){
+
+	}
+	
+	public void updateAlbum(Album album){
+
+	}
+	
+	public void deleteAlbum(Album album){
+
+	}
+	
+
+
+}
