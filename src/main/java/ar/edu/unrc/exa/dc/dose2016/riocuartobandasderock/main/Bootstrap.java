a11bac194b7fa68a489c23035ac2e97b9d44ea36
jheredia <joaquinheredia93@gmail.com>
2016-09-15 19:49:26 -0300

    Routes defined

--- src/main/java/ar/edu/unrc/exa/dc/dose2016/riocuartobandasderock/main/Bootstrap.java
+++ src/main/java/ar/edu/unrc/exa/dc/dose2016/riocuartobandasderock/main/Bootstrap.java
@@ -3,7 +3,16 @@ package ar.edu.unrc.exa.dc.dose2016.riocuartobandasderock.main;
 import static spark.Spark.*;
 
 public class Bootstrap {
+	AlbumController albumController = AlbumController.getInstance();
+	
     public static void main(String[] args) {
+    	
+    	get("/albums", (req, res) -> AlbumController.getAll());
+    	get("/albums/:album_id", (req, res) -> AlbumController.getById(req.params(":album_id")));
+    	post("/albums", (req, res) -> AlbumController.create(req.queryMap()));
+    	put("/albums", (req, res) -> AlbumController.update(req.queryMap()));
+    	delete("/albums/:album_id", (req, res) -> AlbumController.delete(req.params(":album_id")));
         get("/hello", (req, res) -> "Hello World");
+        
     }
 }
