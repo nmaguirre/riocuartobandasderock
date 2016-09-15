829ca647b0fd3dc5b27ace26d694d36307592e79
gabybertorello <gibertorello@gmail.com>
2016-09-15 14:03:30 -0300

    Adding setters and getters of class Album

--- src/main/java/ar/edu/unrc/exa/dc/dose2016/riocuartobandasderock/model/Album.java
+++ src/main/java/ar/edu/unrc/exa/dc/dose2016/riocuartobandasderock/model/Album.java
@@ -17,5 +17,69 @@ public class Album {
 	private List<Song> songs;
 	private int ISRC; 			// international standard recording code
 	private int UPC; 			// universal product code
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public Band getArtist() {
+		return artist;
+	}
+	public void setArtist(Band artist) {
+		this.artist = artist;
+	}
+	public List<String> getProducers() {
+		return producers;
+	}
+	public void setProducers(List<String> producers) {
+		this.producers = producers;
+	}
+	public int getDuration() {
+		return duration;
+	}
+	public void setDuration(int duration) {
+		this.duration = duration;
+	}
+	public List<Style> getStyles() {
+		return styles;
+	}
+	public void setStyles(List<Style> styles) {
+		this.styles = styles;
+	}
+	public String getDiscography() {
+		return discography;
+	}
+	public void setDiscography(String discography) {
+		this.discography = discography;
+	}
+	public List<Song> getSongs() {
+		return songs;
+	}
+	public void setSongs(List<Song> songs) {
+		this.songs = songs;
+	}
+	public int getISRC() {
+		return ISRC;
+	}
+	public void setISRC(int iSRC) {
+		ISRC = iSRC;
+	}
+	public int getUPC() {
+		return UPC;
+	}
+	public void setUPC(int uPC) {
+		UPC = uPC;
+	}
+	
+	
+	
 
 }
