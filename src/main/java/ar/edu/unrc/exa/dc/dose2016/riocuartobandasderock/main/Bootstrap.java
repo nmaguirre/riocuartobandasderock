d2fde197c240c4d74e2b81ca0f427180ee404c94
jheredia <joaquinheredia93@gmail.com>
2016-09-15 19:49:26 -0300

    Routes defined

--- src/main/java/ar/edu/unrc/exa/dc/dose2016/riocuartobandasderock/main/Bootstrap.java
+++ src/main/java/ar/edu/unrc/exa/dc/dose2016/riocuartobandasderock/main/Bootstrap.java
@@ -7,7 +7,20 @@ import ar.edu.unrc.exa.dc.dose2016.riocuartobandasderock.dao.impl.ArtistDaoImpl;
 
 public class Bootstrap {
 	private static ArtistController artistController = new ArtistController(new ArtistDaoImpl());
+	AlbumController albumController = AlbumController.getInstance();
+	
     public static void main(String[] args) {
+    	
+    	get("/albums", (req, res) -> AlbumController.getAll());
+    	
+    	get("/albums/:album_id", (req, res) -> AlbumController.getById(req.params(":album_id")));
+    	
+    	post("/albums", (req, res) -> AlbumController.create(req.queryMap()));
+    	
+    	put("/albums", (req, res) -> AlbumController.update(req.queryMap()));
+    	
+    	delete("/albums/:album_id", (req, res) -> AlbumController.delete(req.params(":album_id")));
+    	
         get("/hello", (req, res) -> "Hello World");
         
         get ("/artist", (req,res)->artistController.getAllArtists(req,res));
