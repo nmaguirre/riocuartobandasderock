26a8b26f577a76344c40a64b7189645ba5fe182d
Mariano Ontivero <orevitnoonairam@yahoo.com.ar>
2016-09-15 11:59:17 -0300

    Adding class Style and instance variables of class Album

--- src/main/java/ar/edu/unrc/exa/dc/dose2016/riocuartobandasderock/model/Album.java
+++ src/main/java/ar/edu/unrc/exa/dc/dose2016/riocuartobandasderock/model/Album.java
@@ -1,5 +1,21 @@
 package ar.edu.unrc.exa.dc.dose2016.riocuartobandasderock.model;
 
+import java.util.List;
+
 public class Album {
+
+	private int id;
+	private String title;
+	private Band artist;
+	private List<String> producers;
+//	private Date releaseDate;
+	private int duration;		// in seconds
+//	private Genre genre;		// genre is given	
+	private List<Style> styles; 
+// private Date recordingDate;
+	private String discography;
+	private List<Song> songs;
+	private int ISRC; 			// international standard recording code
+	private int UPC; 			// universal product code
 
 }
