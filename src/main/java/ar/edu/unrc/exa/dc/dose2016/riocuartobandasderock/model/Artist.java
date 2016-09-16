8a429c43bf676db57231821861df94ad4afe3559
Diana Gonzalez <dianagonzalez22.dg@gmail.com>
2016-09-16 18:47:54 -0300

    [#3] Corrections.

--- src/main/java/ar/edu/unrc/exa/dc/dose2016/riocuartobandasderock/model/Artist.java
+++ src/main/java/ar/edu/unrc/exa/dc/dose2016/riocuartobandasderock/model/Artist.java
@@ -3,108 +3,109 @@ package ar.edu.unrc.exa.dc.dose2016.riocuartobandasderock.model;
 import java.util.List;
 import java.util.UUID;
 
-/* This class represents one person that is member of a band. */
+/* This class represents one person that is artist of a band. */
 public class Artist {
 
   /**
-  * id represents the object band member into the database
-  * name represents the member name
-  * surname represents the member surname
-  * nickname represents the member nickname
-  * role represents the member role in the band
+  * id represents the object band artist into the database
+  * name represents the artist name
+  * surname represents the artist surname
+  * nickname represents the artist nickname
   */
   private String id;
   private String name;
   private String surname;
   private String nickname;
-  private List<String> role;
-  //private List<Role> role; CREAR ENUMERADO DE ROLES.
-
 
 
   /**
-  * The constructor of BandMember class sets the new member without an id.
-  */
-  private Artist(String id, String name, String surname, String nickname, List<String> role){    
+   * The constructor of Artist class sets the new artist
+   * @param id: Artist id.
+   * @param name: Artist name.
+   * @param surname: Artist surname.
+   * @param nickname: Artist nickname.
+   */
+  private Artist(String id, String name, String surname, String nickname){    
     this.id = id;
 	this.name = name;
     this.surname = surname;
     this.nickname = nickname;
-    this.role = role;
   }
   
   /**
-   * The constructor of BandMember class sets the new member without an id.
+   * The constructor of Artist class sets the new artist.
+   * @param name: Artist name.
+   * @param surname: Artist surname.
+   * @param nickname: Artist nickname.
    */
-  public Artist(String name, String surname, String nickname, List<String> role){    
-     this(UUID.randomUUID().toString(), name, surname, nickname, role);
+  public Artist(String name, String surname, String nickname){    
+     this(UUID.randomUUID().toString(), name, surname, nickname);
   }
   
   /**
-  * @return member id.
+   * The constructor of Artist class sets a new artist with a name and surname.
+   * @param name: Artist name.
+   * @param surname: Artist surname.
+   */
+  public Artist(String name, String surname){
+    this(UUID.randomUUID().toString(), name, surname, "");
+  }
+  
+  /**
+  * @return artist id.
   */
   public String getId(){
     return id;
   }
   
   /**
-  * @return member name.
+  * @return artist name.
   */
   public String getName(){
 	  return name;
   }
   
   /**
-  * @return member surname.
+  * @return artist surname.
   */  
   public String getSurname(){
     return surname;
   }
 
   /**
-  * @return member nickname.
+  * @return artist nickname.
   */ 
   public String getNickname(){
     return nickname;
   }
- 
-  /**
-  * @return member role.
-  */
-  public List<String> getRole(){
-    return role;
-  } 
   
   /**
-  * This method sets the member name.
-  * @param bm_name member name.
+  * This method sets the artist name.
+  * @param art_name: Artist name.
+  * @throws IllegalArgumentException if name is null.
   */
-  public void setName(String bm_name){
-	  if (bm_name == null) throw new IllegalArgumentException();
-	  name = bm_name;
+  public void setName(String art_name){
+	  if (art_name == null) throw new IllegalArgumentException("Name can't be null");
+	  name = art_name;
   }
 
   /**
-  * This method sets the member surname.
-  * @param bm_sur member surname.
+  * This method sets the artist surname.
+  * @param art_sur member surname.
+  * @throws IllegalArgumentException if surname is null.
   */
-  public void setSurname(String bm_sur){
-    surname = bm_sur;
+  public void setSurname(String art_sur){
+    if (art_sur == null) throw new IllegalArgumentException("Surname can't be null");
+    surname = art_sur;
   }
 
   /**
-  * This method sets the member nickname.
+  * This method sets the artist nickname.
   * @param bm_nick member name.
+  * @throws IllegalArgumentException if nickname is null.
   */
-  public void setNickname(String bm_nick){
-    nickname = bm_nick;
-  }
-
-  /**
-  * This method sets the member role.
-  * @param bm_role member role.
-  */
-  public void setRole(List<String> bm_role){
-    role = bm_role;
+  public void setNickname(String art_nick){
+    if (art_nick == null) throw new IllegalArgumentException("Nickname can't be null");
+    nickname = art_nick;
   }
 }
