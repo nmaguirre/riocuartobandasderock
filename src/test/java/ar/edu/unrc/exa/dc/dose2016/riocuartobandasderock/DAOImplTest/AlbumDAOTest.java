43de91dace2c9872b5feb7f94b92883ef09bcb76
dmatiasr <matu_dmr@hotmail.com>
2016-09-18 20:10:36 -0300

    AlbumDAOTest.FinByIdTestCase()

--- src/test/java/ar/edu/unrc/exa/dc/dose2016/riocuartobandasderock/DAOImplTest/AlbumDAOTest.java
+++ src/test/java/ar/edu/unrc/exa/dc/dose2016/riocuartobandasderock/DAOImplTest/AlbumDAOTest.java
@@ -6,28 +6,25 @@ import mockit.Mocked;
 
 import org.junit.Test;
 
-import ar.edu.unrc.exa.dc.dose2016.riocuartobandasderock.dao.impl.AlbumDaoImpl;
+import ar.edu.unrc.exa.dc.dose2016.riocuartobandasderock.dao.AlbumDAO;
 import ar.edu.unrc.exa.dc.dose2016.riocuartobandasderock.model.Album;
 
 //Working in this class test
 public class AlbumDAOTest {
 	
-	@Mocked AlbumDaoImpl albumDao;
-	@Mocked Album albumModel;
+	@Mocked AlbumDAO albumDao;
 	
 	//Working in this test
 	@Test
 	public void findByIdTestCase() {
-		albumDao = new AlbumDaoImpl();
-		albumModel = new Album();
-		
+		Album albumModel = new Album();
+		albumModel.setId(3);
 		new Expectations(){
 			{
 				albumDao.findById(withEqual(3));
-				albumModel.setId(3);
 				returns (albumModel);
-				assertEquals(3,albumModel.getId());
 			}
 		};
+		assertEquals(albumModel,albumDao.findById(3));
 	}
 }
