6ae911f84851b95bb0b367bec5212a8f1483c447
Leandro Etcharren <leo_etcharren@hotmail.com>
2016-09-19 17:22:05 -0300

    Agregate declaration methods to BandMemberController

--- src/main/java/ar/edu/unrc/exa/dc/dose2016/riocuartobandasderock/main/BandMemberController.java
+++ src/main/java/ar/edu/unrc/exa/dc/dose2016/riocuartobandasderock/main/BandMemberController.java
@@ -1,5 +1,42 @@
 package ar.edu.unrc.exa.dc.dose2016.riocuartobandasderock.main;
 
+import ar.edu.unrc.exa.dc.dose2016.riocuartobandasderock.model.BandMember;
+import ar.edu.unrc.exa.dc.dose2016.riocuartobandasderock.model.Artist;
+import ar.edu.unrc.exa.dc.dose2016.riocuartobandasderock.model.Band;
+import spark.Response;
+import spark.Request;
+import ar.edu.unrc.exa.dc.dose2016.riocuartobandasderock.dao.BandMemberDAO;
+import java.util.List;
+
 public class BandMemberController {
+	private BandMemberDAO bmDAO;
+
+	public BandMemberController(BandMemberDAO bmDAO_a){
+		bmDAO = bmDAO_a;
+	}
+	
+	public BandMember getBandMember (Request req, Response res){
+		return null;
+	}
+
+	public List<Artist> getBandMembersBand (Request req, Response res){
+		return null; 
+	}
+	
+	public Band getBandsBandMember (Request req, Response res){
+		return null; 
+	}
+		
+	public boolean createBandMember(Request req,Response res){
+		return false;
+	}
+	
+	public boolean updateBandMember(Request req, Response res){
+		return false;
+	}
+	
+	public boolean deleteBandMember(Request req, Response res){
+	 	return false;
+	}
 
 }
