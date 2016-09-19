291fccc06eda3d0c0ea3bda0c1a9156b92ad3a73
Mariano Ontivero <orevitnoonairam@yahoo.com.ar>
2016-09-19 16:31:35 -0300

    Album class constructor and enum class Genre

--- src/main/java/ar/edu/unrc/exa/dc/dose2016/riocuartobandasderock/model/Album.java
+++ src/main/java/ar/edu/unrc/exa/dc/dose2016/riocuartobandasderock/model/Album.java
@@ -1,5 +1,6 @@
 package ar.edu.unrc.exa.dc.dose2016.riocuartobandasderock.model;
 
+import java.util.Date;
 import java.util.List;
 
 public class Album {
@@ -8,78 +9,126 @@ public class Album {
 	private String title;
 	private Band artist;
 	private List<String> producers;
-//	private Date releaseDate;
+	private Date releaseDate;
 	private int duration;		// in seconds
-//	private Genre genre;		// genre is given	
+	private Genre genre;		// genre is given	
 	private List<Style> styles; 
-// private Date recordingDate;
+	private Date recordingDate;
 	private String discography;
 	private List<Song> songs;
 	private int ISRC; 			// international standard recording code
 	private int UPC; 			// universal product code
 	
+	public Album() {
+		
+	}
+	
+	public Album(int id, String title, Band artist) {
+		this.id = id;
+		this.title = title;
+		this.artist = artist;
+	}
+	
+	public Album(int id, String title, Band artist, Date releaseDate, int duration, Genre genre, Date recordingDate,
+			String discography, int ISRC, int UPC) {
+		this.id = id;
+		this.title = title;
+		this.artist = artist;
+		this.releaseDate = releaseDate;
+		this.duration = duration;
+		this.genre = genre;
+		this.recordingDate = recordingDate;
+		this.discography = discography;
+		this.ISRC = ISRC;
+		this.UPC = UPC;
+	}
+
 	public int getId() {
 		return id;
 	}
+	
 	public void setId(int id) {
 		this.id = id;
 	}
+	
 	public String getTitle() {
 		return title;
 	}
+	
 	public void setTitle(String title) {
 		this.title = title;
 	}
+	
 	public Band getArtist() {
 		return artist;
 	}
+	
 	public void setArtist(Band artist) {
 		this.artist = artist;
 	}
+	
 	public List<String> getProducers() {
 		return producers;
 	}
+	
 	public void setProducers(List<String> producers) {
 		this.producers = producers;
 	}
+	
+	public void addProducer(String producer) {
+		this.producers.add(producer);
+	}
+	
 	public int getDuration() {
 		return duration;
 	}
+	
 	public void setDuration(int duration) {
 		this.duration = duration;
 	}
+	
 	public List<Style> getStyles() {
 		return styles;
 	}
+	
 	public void setStyles(List<Style> styles) {
 		this.styles = styles;
 	}
+	
 	public String getDiscography() {
 		return discography;
 	}
+	
 	public void setDiscography(String discography) {
 		this.discography = discography;
 	}
+	
+	public void addSong(Song song, int position) {
+		this.songs.add(position, song);
+	}
+	
 	public List<Song> getSongs() {
 		return songs;
 	}
+	
 	public void setSongs(List<Song> songs) {
 		this.songs = songs;
 	}
+	
 	public int getISRC() {
 		return ISRC;
 	}
+	
 	public void setISRC(int iSRC) {
 		ISRC = iSRC;
 	}
+	
 	public int getUPC() {
 		return UPC;
 	}
+	
 	public void setUPC(int uPC) {
 		UPC = uPC;
 	}
 	
-	
-	
-
 }
---
+++ src/main/java/ar/edu/unrc/exa/dc/dose2016/riocuartobandasderock/model/Genre.java
@@ -0,0 +1,14 @@
+package ar.edu.unrc.exa.dc.dose2016.riocuartobandasderock.model;
+
+public enum Genre {
+		rock,
+		pop,
+		blues,
+		reggae,
+		folk,
+		electronic,
+		metal,
+		punk;
+		// and go on
+		
+}
