13038a802ccf096a7619f95d711e5a084c910f11
agustinmartinez01 <agustinmartinez01@gmail.com>
2016-09-19 18:30:41 -0300

    create test bandDAOTest

---
+++ src/test/java/ar/edu/unrc/exa/dc/dose2016/riocuartobandasderock/DAOImplTest/BandDAOTest.java
@@ -0,0 +1,19 @@
+package ar.edu.unrc.exa.dc.dose2016.riocuartobandasderock.DAOImplTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import ar.edu.unrc.exa.dc.dose2016.riocuartobandasderock.dao.AlbumDAO;
+import ar.edu.unrc.exa.dc.dose2016.riocuartobandasderock.dao.BandDAO;
+import mockit.Mocked;
+
+public class BandDAOTest {
+	@Mocked BandDAO bandDao;
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
