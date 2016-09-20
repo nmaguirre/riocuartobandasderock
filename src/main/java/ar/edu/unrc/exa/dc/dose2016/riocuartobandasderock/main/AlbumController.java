0dc14db4a933bc8fefbcecf83c62eebe72aedc42
Luciano Rocha <rocha.lucho@gmail.com>
2016-09-20 01:09:43 -0300

    Refactor AlbumController.

--- src/main/java/ar/edu/unrc/exa/dc/dose2016/riocuartobandasderock/main/AlbumController.java
+++ src/main/java/ar/edu/unrc/exa/dc/dose2016/riocuartobandasderock/main/AlbumController.java
@@ -1,83 +1,50 @@
 package ar.edu.unrc.exa.dc.dose2016.riocuartobandasderock.main;
 
-import static spark.Spark.get;
-import static spark.Spark.*; 
-import ar.edu.unrc.exa.dc.dose2016.riocuartobandasderock.model.Album;
 import java.util.List;
 
+import ar.edu.unrc.exa.dc.dose2016.riocuartobandasderock.dao.AlbumDAO;
+import ar.edu.unrc.exa.dc.dose2016.riocuartobandasderock.model.Album;
+import spark.QueryParamsMap;
+
 public class AlbumController {
-	
-	public AlbumController(final AlbumDAOImpl albumDAO){
-		
-		get("/albums", (req,res) -> AlbumDAOImpl.getAllAlbums());
-		get("/albums/:album_id", (req,res) -> {
-			String id = req.params(":album_id");
-			Album album = AlbumDAOImpl.findById(Integer.parseInt(id)); 
-			if (album == null) res.status(400);
-			return album;
-		});
-		post("/albums", (req, res) -> {
-			Album album = new Album(); //Params should go here.
-			int status = AlbumDAOImpl.createAlbum(album) ? 200 : 400;
-			res.status(status);
-		});
-		put("/albums/:album_id", (req,res) -> {
-			Album album = AlbumDAOImpl.findById(Integer.parseInt(req.params(":album_id")));
-			album.set
-			int status = AlbumDAOImpl.updateAlbum
-		});
-		/**Not sure if the rest of the searches are necessary,
-		 * maybe we could return the whole collection and let 
-		 * the client filter.
-		 */
-//		get("/albums/:album_name", (req,res) -> {
-//			String album_name = req.params(":album_name");
-//			Album album = AlbumDAOImpl.findByName(album_name);
-//			if (album == null) res.status(400);
-//			return album;
-//		});
-//		get("/albums/:album_year", (req, res) -> {
-//			String album_year = req.params(":album_year");
-//			List<Album> albums = AlbumDAOImpl.findByYear(Integer.parseInt(album_year));
-//			if (albums == null) res.status(400);
-//			return albums;
-//		});
-//		get("/albums/:genre", (req, res) -> {
-//			String genre = req.params(":genre");
-//			List<Album> albums = AlbumDAOImpl.findByGenere(genre);
-//			if (albums == null) res.status(400);
-//			return albums;
-//		});
-//		get("/albums/:record_label", (req, res) -> {
-//			String record_label = req.params(":record_label");
-//			List<Album> albums = AlbumDAOImpl.findByRecordLabel(record_label);
-//			if (albums == null) res.status(400);
-//			return albums;
-//		});
-//		get("/albums/:producer", (req, res) -> {
-//			String producer = req.params(":producer");
-//			List<Album> albums = AlbumDAOImpl.findByProducer(producer);
-//			if (albums == null) res.status(400);
-//			return albums;
-//		});
-//		get("/albums/:duration", (req, res) -> {
-//			
-//		})
-//		get("/bands/:band_name/albums", (req,res) -> {
-//			String band_name = req.params(":band_name");
-//			List<Album> albums = AlbumDAOImpl.findByBandName(band_name);
-//			if (albums == null) res.status(400);
-//			return albums;
-//		});
-//		get("/bands/:band_id/albums", (req,res) -> AlbumDAOImpl.getBandAlbums());
-//		get("/bands/:band_id/albums/:album_id", (req,res) -> {
-//			String band_id = req.params(":band_id");
-//			String album_id = req.params(":album_id");
-//			Album album = AlbumDAOImpl.getBandAlbum(band_id, album_id);
-//			if (album == null) res.status(400);
-//			return album;
-//		});
-		
-		
-	}
+    protected static AlbumController unique_instance;
+    protected AlbumDAO dao;
+
+    protected AlbumController() {
+        unique_instance = new AlbumController();
+    }
+
+    public static AlbumController getInstance() {
+        if (unique_instance == null)
+            unique_instance = new AlbumController();
+        return unique_instance;
+    }
+
+    public List<Album> getAll() {
+        return dao.getAllAlbums();
+    }
+
+    public Album getById(String id) {
+        return dao.findById(Integer.parseInt(id));
+    }
+
+    public Boolean create(QueryParamsMap params) {
+        // Maybe we need to sanitize params
+        // Album album = new Album(params);
+        // return album.save
+        return false;
+    }
+
+    public Boolean update(QueryParamsMap params) {
+        int id = params.get("id").integerValue();
+        // Maybe we need to sanitize params
+        // Album album = dao.findById(id);
+        // return dao.updateAlbum(album, params);
+        return false;
+    }
+
+    public Boolean delete(String id) {
+        dao.deleteAlbum(dao.findById(Integer.parseInt(id)));
+        return false;
+    }
 }
--- src/main/java/ar/edu/unrc/exa/dc/dose2016/riocuartobandasderock/main/Bootstrap.java
+++ src/main/java/ar/edu/unrc/exa/dc/dose2016/riocuartobandasderock/main/Bootstrap.java
@@ -1,17 +1,23 @@
 package ar.edu.unrc.exa.dc.dose2016.riocuartobandasderock.main;
 
-import static spark.Spark.*;
+import static spark.Spark.delete;
+import static spark.Spark.get;
+import static spark.Spark.post;
+import static spark.Spark.put;
 
 public class Bootstrap {
-	AlbumController albumController = AlbumController.getInstance();
+    private static AlbumController albumController = AlbumController.getInstance();
 	
     public static void main(String[] args) {
     	
-    	get("/albums", (req, res) -> AlbumController.getAll());
-    	get("/albums/:album_id", (req, res) -> AlbumController.getById(req.params(":album_id")));
-    	post("/albums", (req, res) -> AlbumController.create(req.queryMap()));
-    	put("/albums", (req, res) -> AlbumController.update(req.queryMap()));
-    	delete("/albums/:album_id", (req, res) -> AlbumController.delete(req.params(":album_id")));
+        /* Start album routes */
+        get("/albums", (req, res) -> albumController.getAll());
+        get("/albums/:album_id", (req, res) -> albumController.getById(req.params(":album_id")));
+        post("/albums", (req, res) -> albumController.create(req.queryMap()));
+        put("/albums", (req, res) -> albumController.update(req.queryMap()));
+        delete("/albums/:album_id", (req, res) -> albumController.delete(req.params(":album_id")));
+        /* End album routes */
+
         get("/hello", (req, res) -> "Hello World");
         
     }
