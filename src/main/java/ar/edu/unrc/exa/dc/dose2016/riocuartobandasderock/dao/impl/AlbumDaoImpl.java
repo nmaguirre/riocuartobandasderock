0fe1708be917c6939ba372825eba88cfc1d9547a
MauriDV <mdellevedove12@gmail.com>
2016-09-30 19:02:17 -0300

    add javaDoc comments

--- src/main/java/ar/edu/unrc/exa/dc/dose2016/riocuartobandasderock/dao/impl/AlbumDaoImpl.java
+++ src/main/java/ar/edu/unrc/exa/dc/dose2016/riocuartobandasderock/dao/impl/AlbumDaoImpl.java
@@ -21,6 +21,7 @@ public class AlbumDaoImpl implements AlbumDAO{
 	public Album findById(String id){
 		return null;
 	}
+	
 	/**
 	 * @return list of albums contained
 	 */
@@ -43,6 +44,7 @@ public class AlbumDaoImpl implements AlbumDAO{
 	public Album findByName(String name){
 		return null;
 	}
+	
 	/**
 	 * @param genere
 	 * @return List of albums found by genere
@@ -50,35 +52,67 @@ public class AlbumDaoImpl implements AlbumDAO{
 	public List<Album> findByGenere(String genere){
 		return null;
 	}
-
+	
+	/**
+	 * @param recordLabel
+	 * @return List of albums found by record label
+	 */
 	public List<Album> findByRecordLabel(String recordLabel){
 		return null;
 	}
 	
+	/**
+	 * @param producer
+	 * @return List of albums found by producer
+	 */
 	public List<Album> findByProducer(String producer){
 		return null;
 	}
 	
+	/**
+	 * @param duration
+	 * @return List of albums found by duration
+	 */
 	public List<Album> findByDuration(int duration){
 		return null;
 	}
 	
+	/**
+	 * @param song
+	 * @return List of albums found by song
+	 */
 	public List<Album> findBySong(String song){
 		return null;
 	}
 	
+	/**
+	 * @param producer
+	 * @return List of albums found by producer
+	 */
 	public List<Album> findByReleaseDate(int year){
 		return null;
 	}
-
+	
+	/**
+	 * @param album
+	 * @return true if album was inserted into data base correctly
+	 */
 	public boolean createAlbum(Album album){
 		return false;
 	}
 	
+	/**
+	 * @param album
+	 * @return true if the album was updated correctly
+	 */
 	public boolean updateAlbum(Album album){
 		return false;
 	}
 	
+	/**
+	 * @param id
+	 * @return true if the album was removed from de data base correctly
+	 */
 	public boolean deleteAlbum(String id){
 		return false;
 	}
