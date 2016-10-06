19dedca864cce09bf57b451f9644611566c697fe
Simon Emmanuel Gutierrez Brida <simon.gutierrez.brida@gmail.com>
2016-10-06 19:12:37 -0300

    Fixed some scripts for linux with no docker

--- src/test/resources/acceptance_tests/kill_all_acceptance_tests_db_containers_no_docker.sh
+++ src/test/resources/acceptance_tests/kill_all_acceptance_tests_db_containers_no_docker.sh
@@ -1,3 +1,6 @@
 #This script should stop the database
+new_host=localhost
+new_port=5432
 
-echo "all databases killed..."
+dropdb -h $new_host -p $new_port -U postgres --if-exists rcrockbands
+dropuser -h $new_host -p $new_port -U postgres --if-exists rock_db_owner
