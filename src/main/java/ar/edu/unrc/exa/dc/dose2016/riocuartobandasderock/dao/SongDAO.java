010e6932e622018870f949659aae8e31e778357f
jortolano <ortolano33@gmail.com>
2016-10-13 18:54:02 -0300

    refactoring of the songDAO class and songDAOImpl

--- src/main/java/ar/edu/unrc/exa/dc/dose2016/riocuartobandasderock/dao/SongDAO.java
+++ src/main/java/ar/edu/unrc/exa/dc/dose2016/riocuartobandasderock/dao/SongDAO.java
@@ -2,11 +2,28 @@ package ar.edu.unrc.exa.dc.dose2016.riocuartobandasderock.dao;
 
 import java.util.List;
 
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 import ar.edu.unrc.exa.dc.dose2016.riocuartobandasderock.model.Song;
 
 public interface SongDAO {
 
+	public Session openCurrentSession();
+
+	public Session openCurrentSessionwithTransaction();
+
+	public void closeCurrentSession();
+
+	public void closeCurrentSessionwithTransaction();
+
+	public Session getCurrentSession();
+
+	public void setCurrentSession(Session currentSession);
+	
+	public Transaction getCurrentTransaction();
+
+	public void setCurrentTransaction(Transaction currentTransaction);
 	
 	public List<Song> getAllSongs();
 	   
@@ -20,11 +37,11 @@ public interface SongDAO {
 	
 	public List<Song> findByBandName(String BandName);
 	
-                    public List<Song> findByGenere(String genere);
+    public List<Song> findByGenere(String genere);
                     
-                    public List<Song> findByAuthor(String author);
+    public List<Song> findByAuthor(String author);
                     
-                    public List<Song> findByAlbum(String album);
+    public List<Song> findByAlbum(String album);
                     
 	public List<Song> findByName(String name);
 	
