0b8fbbe629b19ee2e4eec723813128b378f86fee
jheredia <joaquinheredia93@gmail.com>
2016-10-18 18:29:28 -0300

    Modified controller to catch exceptions

--- src/main/java/ar/edu/unrc/exa/dc/dose2016/riocuartobandasderock/main/AlbumController.java
+++ src/main/java/ar/edu/unrc/exa/dc/dose2016/riocuartobandasderock/main/AlbumController.java
@@ -1,5 +1,7 @@
 package ar.edu.unrc.exa.dc.dose2016.riocuartobandasderock.main;
 
+import java.text.DateFormat;
+import java.text.ParseException;
 import java.util.Date;
 import java.util.List;
 
@@ -24,17 +26,29 @@ public class AlbumController {
     }
 
     public Boolean create(Request req, Response res) {
-    	if (req.params("name") == null && req.params("release_date") == null){
+    	if (req.queryParams("name") == null && req.queryParams("release_date") == null){
+    		//If both parameters are non existant, return false and a bad request.
     		res.status(400);
+    		res.body("Both params can't be null");
     		return false;
     	}
-    	Date release_date = new Date(req.params("release_date"));
-    	boolean result = dao.createAlbum(req.params("name"), release_date);
-    	//boolean result = dao.createAlbum(new Album());
-    	int http_status = result ? 201 : 409; 
-    	res.status(http_status);
-    	if (!result) res.body("Duplicate album");
-    	return result;
+    	DateFormat df = DateFormat.getDateInstance();
+		try {
+			//Date should be in the next pattern: dd/mm/yyyy
+			Date release_date = df.parse(req.queryParams("release_date"));
+			boolean result = dao.createAlbum(req.params("name"), release_date);
+	    	int http_status = result ? 201 : 409; 
+	    	res.status(http_status);
+	    	if (!result) res.body("Duplicate album"); //If the result of the creation was false, it means that there is a duplicate
+	    	return result;
+		} catch (ParseException e) {
+			//If an exception was thrown, then there was a problem with the parameters.
+			e.printStackTrace();
+			res.status(400);
+			res.body("Bad date");
+			return false;
+		}
+    	
     }
 
 }
