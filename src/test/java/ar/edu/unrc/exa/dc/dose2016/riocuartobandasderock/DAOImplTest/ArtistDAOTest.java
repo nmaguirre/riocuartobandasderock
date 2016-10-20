026b93d75a071d4294ab2aa2973ca777fa1183f0
adriangalfioni <adrian.galfioni@gmail.com>
2016-10-20 19:38:39 -0300

    ArtistDAOTest done

--- src/test/java/ar/edu/unrc/exa/dc/dose2016/riocuartobandasderock/DAOImplTest/ArtistDAOTest.java
+++ src/test/java/ar/edu/unrc/exa/dc/dose2016/riocuartobandasderock/DAOImplTest/ArtistDAOTest.java
@@ -25,6 +25,10 @@ public class ArtistDAOTest {
 		artistDAO = new ArtistDaoImpl();
 	}
 	
+	/*
+	 * CREATE ARTIST METHOD TESTS
+	 */
+	
 	@Test
 	public void createArtistTest_Artist_not_in_db() {
 		
@@ -146,5 +150,248 @@ public class ArtistDAOTest {
 		artistDAO.closeCurrentSessionwithTransaction();
 	}
 	
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void createArtistTest_Artist_with_empty_fields() {
+		String name = "";
+		String surname = "";
+		String nickname = "";
+		
+		// Create the artist to add in db
+		Artist artistToAdd = new Artist(name, surname, nickname);
+		
+		// Add artistToAdd in db
+		artistDAO.openCurrentSessionwithTransaction();
+		artistDAO.createArtist(name, surname, nickname);
+		artistDAO.closeCurrentSessionwithTransaction();
+	}
+	
+	
+	/*
+	 * FIND BY NAME METHOD TESTS
+	 */
+	
+	@Test
+	public void findByName_Artist_in_db() {
+		List<Artist> artistList = new LinkedList<>();
+		
+		String name = "a";
+		String surname = "b";
+		String nickname = "";
+		
+		artistDAO.openCurrentSession();
+		while(artistDAO.existArtist(name,surname,nickname)){
+			name+="a";
+		}
+		artistDAO.closeCurrentSession();
+		
+		// Create the artist to add in db
+		Artist artistToAdd = new Artist(name, surname, nickname);
+		
+		// Add artistToAdd in db
+		artistDAO.openCurrentSessionwithTransaction();
+		artistDAO.createArtist(name,surname,nickname);
+		artistDAO.closeCurrentSessionwithTransaction();
+		
+		artistDAO.openCurrentSession();
+		List<Artist> obtained = artistDAO.findByName(name);
+		artistDAO.closeCurrentSession();
+		
+		assertEquals(obtained.get(0).getName(),name);
+	}
+	
+	@Test
+	public void findByName_Artist_not_in_db() {
+		
+		String name = "i_will_not_find_u";
+		String surname = "";
+		String nickname = "";
+		
+		artistDAO.openCurrentSession();
+		while(artistDAO.existArtist(name,surname,nickname)){
+			name+="a";
+		}
+		artistDAO.closeCurrentSession();
+		
+		artistDAO.openCurrentSession();
+		List<Artist> obtained = artistDAO.findByName(name);
+		artistDAO.closeCurrentSession();
+		
+		assertEquals(obtained.size(), 0);
+	}
+	
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void findByName_null_name() {
+		
+		String name = null;
+		
+		artistDAO.openCurrentSession();
+		List<Artist> obtained = artistDAO.findByName(name);
+		artistDAO.closeCurrentSession();
+	}
+	
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void findByName_empty_name() {
+		
+		String name = "";
+		
+		artistDAO.openCurrentSession();
+		List<Artist> obtained = artistDAO.findByName(name);
+		artistDAO.closeCurrentSession();
+	}
+	
+	
+	/*
+	 * FIND BY SURNAME METHOD TESTS
+	 */
+	
+	@Test
+	public void findBySurname_Artist_in_db() {
+		List<Artist> artistList = new LinkedList<>();
+		
+		String name = "a";
+		String surname = "b";
+		String nickname = "";
+		
+		artistDAO.openCurrentSession();
+		while(artistDAO.existArtist(name,surname,nickname)){
+			surname+="b";
+		}
+		artistDAO.closeCurrentSession();
+		
+		// Create the artist to add in db
+		Artist artistToAdd = new Artist(name, surname, nickname);
+		
+		// Add artistToAdd in db
+		artistDAO.openCurrentSessionwithTransaction();
+		artistDAO.createArtist(name,surname,nickname);
+		artistDAO.closeCurrentSessionwithTransaction();
+		
+		artistDAO.openCurrentSession();
+		List<Artist> obtained = artistDAO.findBySurname(surname);
+		artistDAO.closeCurrentSession();
+		
+		assertEquals(obtained.get(0).getSurname(),surname);
+	}
+	
+	@Test
+	public void findBySurname_Artist_not_in_db() {
+		
+		String name = "";
+		String surname = "i_will_not_find_u";
+		String nickname = "";
+		
+		artistDAO.openCurrentSession();
+		while(artistDAO.existArtist(name,surname,nickname)){
+			surname+="a";
+		}
+		artistDAO.closeCurrentSession();
+		
+		artistDAO.openCurrentSession();
+		List<Artist> obtained = artistDAO.findBySurname(surname);
+		artistDAO.closeCurrentSession();
+		
+		assertEquals(obtained.size(), 0);
+	}
+	
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void findBySurname_null_surname() {
+		
+		String surname = null;
+		
+		artistDAO.openCurrentSession();
+		List<Artist> obtained = artistDAO.findBySurname(surname);
+		artistDAO.closeCurrentSession();
+	}
+	
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void findBySurname_empty_surname() {
+		
+		String surname = "";
+		
+		artistDAO.openCurrentSession();
+		List<Artist> obtained = artistDAO.findBySurname(surname);
+		artistDAO.closeCurrentSession();
+	}
+	
+	
+	/*
+	 * FIND BY NICKNAME METHOD TESTS
+	 */
+	
+	@Test
+	public void findByNickname_Artist_in_db() {
+		List<Artist> artistList = new LinkedList<>();
+		
+		String name = "a";
+		String surname = "b";
+		String nickname = "c";
+		
+		artistDAO.openCurrentSession();
+		while(artistDAO.existArtist(name,surname,nickname)){
+			nickname+="a";
+		}
+		artistDAO.closeCurrentSession();
+		
+		// Create the artist to add in db
+		Artist artistToAdd = new Artist(name, surname, nickname);
+		
+		// Add artistToAdd in db
+		artistDAO.openCurrentSessionwithTransaction();
+		artistDAO.createArtist(name,surname,nickname);
+		artistDAO.closeCurrentSessionwithTransaction();
+		
+		artistDAO.openCurrentSession();
+		List<Artist> obtained = artistDAO.findByNickname(nickname);
+		artistDAO.closeCurrentSession();
+		
+		assertEquals(obtained.get(0).getName(),name);
+	}
+	
+	@Test
+	public void findByNnickname_Artist_not_in_db() {
+		
+		String name = "";
+		String surname = "";
+		String nickname = "i_will_not_find_u";
+		
+		artistDAO.openCurrentSession();
+		while(artistDAO.existArtist(name,surname,nickname)){
+			nickname+="a";
+		}
+		artistDAO.closeCurrentSession();
+		
+		artistDAO.openCurrentSession();
+		List<Artist> obtained = artistDAO.findByNickname(nickname);
+		artistDAO.closeCurrentSession();
+		
+		assertEquals(obtained.size(), 0);
+	}
+	
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void findByNickname_null_nickname() {
+		
+		String nickname = null;
+		
+		artistDAO.openCurrentSession();
+		List<Artist> obtained = artistDAO.findByNickname(nickname);
+		artistDAO.closeCurrentSession();
+	}
+	
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void findByNickname_empty_nickname() {
+		
+		String nickname = "";
+		
+		artistDAO.openCurrentSession();
+		List<Artist> obtained = artistDAO.findByNickname(nickname);
+		artistDAO.closeCurrentSession();
+	}
 	
 }
