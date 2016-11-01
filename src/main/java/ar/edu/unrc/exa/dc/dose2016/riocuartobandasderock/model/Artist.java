c45e0936a947f5b16d940977ade322376781a34d
Diana Gonzalez <dianagonzalez22.dg@gmail.com>
2016-11-01 18:52:27 -0300

    [#75] Resolved.

--- src/main/java/ar/edu/unrc/exa/dc/dose2016/riocuartobandasderock/model/Artist.java
+++ src/main/java/ar/edu/unrc/exa/dc/dose2016/riocuartobandasderock/model/Artist.java
@@ -59,7 +59,7 @@ public class Artist {
    * @param nickname: Artist nickname.
    */
   public Artist(String name, String surname, String nickname){    
-     this(UUID.randomUUID().toString(), name, surname, nickname);
+	this(UUID.randomUUID().toString(), name, surname, nickname);
   }
   
   /**
@@ -83,7 +83,7 @@ public class Artist {
   * @return artist name.
   */
   public String getName(){
-	  return name;
+	return name;
   }
   
   /**
@@ -106,8 +106,8 @@ public class Artist {
   * @throws IllegalArgumentException if name is null.
   */
   public void setName(String art_name){
-	  if (art_name == null) throw new IllegalArgumentException("Name can't be null");
-	  name = art_name;
+	if (art_name == null) throw new IllegalArgumentException("Name can't be null");
+	name = art_name;
   }
 
   /**
@@ -136,24 +136,24 @@ public class Artist {
    * @throws IllegalArgumentException if art is null.
    */
   public boolean equals(Artist art){
-	  if(art==null) throw new IllegalArgumentException("Artist can't be null");
-	  if(this.name.equals(art.name) && this.nickname.equals(art.nickname) && this.surname.equals(art.surname)){
-		  return true;
-	  }
-	  return false;
-  }
+	if(art==null) throw new IllegalArgumentException("Artist can't be null");
+	if(this.name.equals(art.name) && this.nickname.equals(art.nickname) && this.surname.equals(art.surname)){
+		return true;
+	}
+	return false;
+  } 
   
   /**
    * @return true if the fields are not empty at the same time.
    */
   public boolean repOk(){
-		boolean result = true;
-		if (name == null) name = "";
-		if (nickname == null) nickname = "";
-		if (surname == null) surname = "";
-		if ((name == "") && (nickname == "") && (surname == "")){
-			result = false;
-		}
-		return result;
+	boolean result = true;
+	if (name == null) name = "";
+	if (nickname == null) nickname = "";
+	if (surname == null) surname = "";
+	if ((name == "") && (nickname == "") && (surname == "")){
+		result = false;
 	}
+	return result;
+  }
 }
