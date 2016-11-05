5fb2427e1cd14226637785d3249757c10711b446
Emiliano Guttlein <emilianog09@gmail.com>
2016-11-05 18:09:38 -0300

    Added some test for SessionManager Unit Test
    Added few tests for the SessionManager Class. A lot more tests are
    needed on this Unit test

--- src/test/java/ar/edu/unrc/exa/dc/dose2016/riocuartobandasderock/modelTest/SessionManagerTest.java
+++ src/test/java/ar/edu/unrc/exa/dc/dose2016/riocuartobandasderock/modelTest/SessionManagerTest.java
@@ -9,9 +9,27 @@ import ar.edu.unrc.exa.dc.dose2016.riocuartobandasderock.dao.impl.SessionManager
 public class SessionManagerTest {
 
 	@Test
-	public void openCurrentSessionTest(){
+	/* If the session is created then it´s not null */
+	public void notNullSessionTest(){
 		SessionManager session =SessionManager.getInstance();
-		
+		assertNotEquals(null, session.getCurrentSession());
 	}
 	
+	@Test
+	/* If the session is closed then it´s null */
+	//CHECK IF IT´S CORRECT WHAT I´M ASSUMING.
+	public void NullSessionTest(){
+		SessionManager session =SessionManager.getInstance();
+		session.closeCurrentSession();
+		assertEquals(null, session.getCurrentSession());
+	}
+	
+	@Test
+	/* */
+	public void closeAndOpenCheckSessionTest(){
+		SessionManager session =SessionManager.getInstance();
+		session.closeCurrentSession();
+		assertFalse(session.getCurrentSession().isOpen());
+		
+	}
 }
