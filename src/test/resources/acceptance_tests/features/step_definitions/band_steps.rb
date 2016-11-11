a2e6588ee1a397b1c7b7893a44001cc2f928b3a0
Ezequiel Depetris <ezedepetris@gmail.com>
2016-11-11 20:04:24 -0300

    find by on band fixed

--- src/test/resources/acceptance_tests/features/step_definitions/band_steps.rb
+++ src/test/resources/acceptance_tests/features/step_definitions/band_steps.rb
@@ -66,7 +66,7 @@ end
 
 When(/^I search a band with name "([^"]*)" and the band exist in the database$/) do |name|
   begin
-   response = RestClient.post "http://localhost:4567/bands/:#{name}"
+   response = RestClient.get "http://localhost:4567/bands/:#{name}"
    rescue RestClient::Conflict => e
    expect(e.response.code).to eq(201)
   end
@@ -140,18 +140,16 @@ Then(/^the bands' database should have (\d+) entries$/) do |arg1|
 end
 
 Then(/^the band with name "([^"]*)" and genre "([^"]*)" should be on bands' database$/)do |name, genre|
-  begin
-    response = RestClient.get "http://localhost:4567/bands/", { :name => "#{name}", :genre => "#{genre}"}, :content_type => 'text/plain'
-    expect(e.response).to eq(201)
-  end
+
+  result = `psql -h #{HOST} -p #{PORT} -U rock_db_owner -d rcrockbands -c \"select count(*) from bandDB where name = '#{name}' AND genre = '#{genre}';\" -t`
+  result = result.gsub(/[^[:print:]]|\s/,'') # removing non printable chars
+  expect(result).to eq("1")
 end
 
 Then(/^the band with name "([^"]*)" and genre "([^"]*)" shouldn't be on bands' database$/)do |name, genre|
-  begin
-    response = RestClient.get "http://localhost:4567/bands/", { :name => "#{name}", :genre => "#{genre}"}, :content_type => 'text/plain'
-    rescue RestClient::Conflict => e
-    expect(e.response).to eq(304)
-  end
+  result = `psql -h #{HOST} -p #{PORT} -U rock_db_owner -d rcrockbands -c \"select count(*) from bandDB where name = '#{name}' AND genre = '#{genre}';\" -t`
+  result = result.gsub(/[^[:print:]]|\s/,'') # removing non printable chars
+  expect(result).to eq("0")
 end
 
 Then(/^the band with name "([^"]*)" should be on bands' database$/)do |name|
@@ -163,11 +161,9 @@ Then(/^the band with name "([^"]*)" should be on bands' database$/)do |name|
 end
 
 Then(/^the band with name "([^"]*)" shouldn't be on bands' database$/)do |name|
-  begin
-    response = RestClient.get "http://localhost:4567/bands/:#{name}"
-    rescue RestClient::Conflict => e
-    expect(e.response).to eq(304)
-  end
+  result = `psql -h #{HOST} -p #{PORT} -U rock_db_owner -d rcrockbands -c \"select count(*) from bandDB where name = '#{name}';\" -t`
+  result = result.gsub(/[^[:print:]]|\s/,'') # removing non printable chars
+  expect(result).to eq("0")
 end
 
 Then(/^the band with genre "([^"]*)" should be on bands' database$/)do |genre|
