60d2ad9eab60265f13e5e6514bf3c6d44edbc4ec
Diana Gonzalez <dianagonzalez22.dg@gmail.com>
2016-11-14 17:17:55 -0300

    Documentation

--- src/main/java/ar/edu/unrc/exa/dc/dose2016/riocuartobandasderock/dao/impl/ArtistDaoImpl.java
+++ src/main/java/ar/edu/unrc/exa/dc/dose2016/riocuartobandasderock/dao/impl/ArtistDaoImpl.java
@@ -20,14 +20,18 @@ public class ArtistDaoImpl implements ArtistDAO {
 
 	private Session currentSession=null;
 	
+	/*
+	 * This method initializes the session.
+	 * @param Session session, current session.
+	 */
 	public ArtistDaoImpl(Session session) {
 		this.currentSession = session;
 	}
+	
 	/**
-	 * Get all artists from the database
-	 * 
-	 * @return list with all found artists
-	 */
+	 * This method get all artists
+	 * @return List of artists
+	*/
 	@Override
 	public List<Artist> getAllArtists() {
 		List<Artist> artistList = new LinkedList<>();
@@ -38,11 +42,10 @@ public class ArtistDaoImpl implements ArtistDAO {
 	}
 
 	/**
-	 * Search an artist in database
-	 * 
-	 * @param artist surname to search
-	 * @return artist wanted
-	 */
+	 * This method find an artist in the database, by surname.
+	 * @param String surname.
+	 * @return Artists that have a particular surname
+	*/
 	@Override
 	public List<Artist> findBySurname(String surname) {
 		if(surname == null || surname.equals("")){
@@ -55,11 +58,10 @@ public class ArtistDaoImpl implements ArtistDAO {
 	}
 	
 	/**
-	 * Search an artist in database
-	 * 
-	 * @param artist nickname to search
-	 * @return artist wanted
-	 */
+	 * This method find an artist in the database, by nickname.
+	 * @param String nickname.
+	 * @return Artists that have a particular nickname
+	*/
 	@Override
 	public List<Artist> findByNickname(String nickname) {
 		if(nickname == null || nickname.equals("")){
@@ -89,13 +91,10 @@ public class ArtistDaoImpl implements ArtistDAO {
 	}
 	
 	/**
-	 * 
-	 * @param name
-	 * @param surname
-	 * @param nickname
-	 * 
-	 * @return true if this Artist exists in the database.
-	 */
+	 * This method find an artist in the database, by name.
+	 * @param String name
+	 * @return Artists that have a particular name
+	*/
 	@Override
 	public boolean existArtist(String name, String surname, String nickname){
 		boolean result = false;
@@ -124,14 +123,12 @@ public class ArtistDaoImpl implements ArtistDAO {
 	}
 
 	/**
-	 * Create an artist in the database
-	 * 
-	 * @param name from an artist
-	 * @param nickname from an artist
-	 * @param surname from an artist
-	 * 
-	 * @return boolean, true if the artist was created
-	 */
+	 * This method create an artist in the database.
+	 * @param String name.
+	 * @param String nickname.
+	 * @param String surname.
+	 * @return true if the create was successful
+	*/
 	@Override
 	public boolean createArtist(String name, String surname, String nickname) {
 		boolean result;
@@ -157,10 +154,10 @@ public class ArtistDaoImpl implements ArtistDAO {
 	}
 
 	/**
-	 * 
-	 * @param String id
+	 * This method find an artist in the database, by id.
+	 * @param String id.
 	 * @return Artist that have a particular id	
-	 */
+	*/
 	@Override
 	public List<Artist> findById(String id) {
 		if(id == null || id.equals("")){
@@ -174,14 +171,13 @@ public class ArtistDaoImpl implements ArtistDAO {
 	}
 
 	/**
-	 * Update an artist in database
-	 * 
-	 * @param id
-	 * @param name
-	 * @param surname
-	 * @param nickname
-	 * @return true if the update was successful
-	 */
+	 * This method update an artist in database.
+	 * @param String id.
+	 * @param String name.
+	 * @param String surname.
+	 * @param String nickname.
+	 * @return true if the update was successful.
+	*/
 	@Override
 	public boolean updateArtist(String id, String name, String surname, String nickname) {
 		boolean result = true;
@@ -215,10 +211,10 @@ public class ArtistDaoImpl implements ArtistDAO {
 	}
 
 	/**
-	 * 
-	 * @param String id
-	 * @return true if the delete was successful
-	 */
+	 * This method delete an artist in database.
+	 * @param String id.
+	 * @return true if the delete was successful.
+	*/
 	@Override
 	public boolean deleteArtist(String id) {
 		if(id == null || id.equals("")){
@@ -237,13 +233,12 @@ public class ArtistDaoImpl implements ArtistDAO {
 	}
 	
 	/**
-	 * Search for an artist by its parameters
-	 * 
-	 * @param name
-	 * @param surname
-	 * @param nickname
-	 * @return list with the artist wanted, null if artist not found
-	 */
+	 * This method search for an artist by its parameters.
+	 * @param String name.
+	 * @param String surname.
+	 * @param String nickname.
+	 * @return list with the artist wanted, null if artist not found.
+	*/
 	@Override
 	public List<Artist> getArtist(String name, String surname, String nickname) {
 		List<Artist> result=null;
