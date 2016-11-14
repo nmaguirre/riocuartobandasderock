bd4080f961f1a7706078b26f281eaa90b4e31b86
Adrian <adrian.galfioni@gmail.com>
2016-11-14 18:52:15 -0300

    Added artist remove test (ckeck cascade delete)

--- src/test/java/ar/edu/unrc/exa/dc/dose2016/riocuartobandasderock/DAOImplTest/BandMemberDAOTest.java
+++ src/test/java/ar/edu/unrc/exa/dc/dose2016/riocuartobandasderock/DAOImplTest/BandMemberDAOTest.java
@@ -683,4 +683,74 @@ public class BandMemberDAOTest {
 		session.getTransaction().commit();
 	}
 	
+	
+	/*
+	 * DELETE ARTIST AND CHECK IF BANDMEMBER
+	 * WITH ARTIST ID WAS DELETED
+	 */
+	
+	@Test
+	public void artist_deleted() {
+		
+		/*
+		 * CREATE ARTIST IN DB
+		*/
+		
+		String name = "a";
+		String surname = "b";
+		String nickname = "";
+		
+		while(artistDAO.existArtist(name,surname,nickname)){
+			name+="a";
+		}	
+		
+		// Add artistToAdd in db
+		session.beginTransaction();
+		artistDAO.createArtist(name,surname,nickname);
+		session.getTransaction().commit();
+		
+		/*
+		 * CREATE BAND IN DB
+		*/
+		
+		String bandName = "a";
+		String genre = "b";		
+		
+		while(bandDAO.existBand(bandName)){
+			bandName+="a";
+		}		
+		
+		// Add band in db
+		session.beginTransaction();
+		bandDAO.createBand(bandName, genre);
+		session.getTransaction().commit();
+		
+		/*
+		 * CREATE BANDMEMBER IN DB
+		 * (Artist and Band is recent created, then
+		 * the bandmember with their ids isnt in bd)
+		*/			
+		
+		String artistId = artistDAO.getArtist(name, surname, nickname).get(0).getId();
+		
+		String bandId = bandDAO.findByNameAndGenre(bandName, genre).get(0).getId();
+		
+		// Add bandMember in db
+		session.beginTransaction();
+		bandMemberDAO.createBandMember(bandId,artistId);
+		session.getTransaction().commit();
+	
+		
+		// Delete artist
+		session.beginTransaction();
+		artistDAO.deleteArtist(artistId);
+		session.getTransaction().commit();
+		
+		// Bandmember mustnt exists because artist Id is a foreign key with cascade on delete
+		boolean bandMemberExistsinBd = bandMemberDAO.existBandMember(bandId,artistId); 
+		 	
+		// Check that in db the bandMember was deleted
+		assertTrue(!bandMemberExistsinBd);
+	}
+	
 }
