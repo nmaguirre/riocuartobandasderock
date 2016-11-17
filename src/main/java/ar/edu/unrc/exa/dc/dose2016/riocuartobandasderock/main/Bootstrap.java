97e01894005fb8a14aa45c3541ca35efd09c9bbe
Ezequiel Depetris <ezedepetris@gmail.com>
2016-11-17 15:41:16 -0300

    add landing page controller, do a '/' route for landing page and add template velocity engine

--- src/main/java/ar/edu/unrc/exa/dc/dose2016/riocuartobandasderock/main/Bootstrap.java
+++ src/main/java/ar/edu/unrc/exa/dc/dose2016/riocuartobandasderock/main/Bootstrap.java
@@ -34,7 +34,7 @@ public class Bootstrap {
 	private static AlbumController albumController;
     private static UserController userController;
     private static SongController songController;
-	private static String view = "src/main/WebApp/views/";
+    private static LandingPageController landingPageController;
 
     public static void main(String[] args) {
 
@@ -87,6 +87,7 @@ public class Bootstrap {
         bands = BandController.getInstance();
         songController = new SongController();
         userController = UserController.getInstance();
+        landingPageController = LandingPageController.getInstance();
         port(Integer.parseInt(ar.edu.unrc.exa.dc.dose2016.riocuartobandasderock.main.ServerOptions.getInstance().getAppPort()));
 
         before("/bands", (req, res) -> {
@@ -96,8 +97,14 @@ public class Bootstrap {
         });
 
         // List of route and verbs API REST
-        get("/", (req, res) -> new ModelAndView(null,"/views/landing_page/index.mustache"), new MustacheTemplateEngine());
-        
+        // get("/", (req, res) -> {
+        //     Map<String, Object> model = new HashMap<>();
+        //     return new ModelAndView(model, "landing_page/index.vm");
+        // }, new VelocityTemplateEngine()
+        // );
+
+        get("/", (req, res) -> landingPageController.index(req,res), new VelocityTemplateEngine());
+
         post("/albums", (req, res) -> albumController.create(req, res));
                
         get("/albums", (req, res) -> albumController.getAll(req, res));
@@ -154,7 +161,7 @@ public class Bootstrap {
 
         delete("/songs/:id",(req, res) -> songController.remove(req, res));
 
-        after((req, res) -> {res.type("application/json");});
+        after((req, res) -> {res.type("text/html");});
 
     }
 }
---
+++ src/main/java/ar/edu/unrc/exa/dc/dose2016/riocuartobandasderock/main/LandingPageController.java
@@ -0,0 +1,55 @@
+package ar.edu.unrc.exa.dc.dose2016.riocuartobandasderock.main;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import static spark.Spark.*;
+
+import spark.ModelAndView;
+
+import spark.Request;
+import spark.Response;
+
+import spark.ModelAndView;
+import spark.TemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+/***
+ *
+ * @author DOSE
+ * This class implements the communication layer between the persistence and frontend
+ * following the singleton patter.
+ */
+
+public class LandingPageController {
+  /*
+   * check that have only one instance of class
+   */
+  private static LandingPageController instance = null;
+
+  /***
+   * Constructor of class LandingPageController
+   * Implement the singleton pattern.
+   */
+  public static LandingPageController getInstance() {
+      if(instance == null) {
+         instance = new LandingPageController();
+      }
+      return instance;
+  }
+
+  /***
+   * This method returns a index view
+   * @param req
+   * @param res
+   */
+  public ModelAndView index(Request req ,Response res){
+    Map<String, Object> attributes = new HashMap<>();
+    return new ModelAndView(attributes, "landing_page/index.vm");
+  }
+}
