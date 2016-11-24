164c6e71b06ca19104597ae74132bc796e9feead
Emiliano Guttlein <emilianog09@gmail.com>
2016-11-24 19:41:23 -0300

    finally did it the arborJs

--- src/main/webapp/javascript/mainArbor.js
+++ src/main/webapp/javascript/mainArbor.js
@@ -128,13 +128,14 @@
     var sys = arbor.ParticleSystem(1000, 600, 0.5) // create the system with sensible repulsion/stiffness/friction
     sys.parameters({gravity:true}) // use center-gravity to make the graph settle nicely (ymmv)
     sys.renderer = Renderer("#viewport") // our newly created renderer will have its .init() method called shortly by sys...
+    var animals = sys.addNode('Animals',{'color':'red','shape':'dot','label':'Animals'});
 
     // add some nodes to the graph and watch it go...
-    sys.addEdge('a','b')
-    sys.addEdge('a','c')
-    sys.addEdge('a','d')
-    sys.addEdge('a','e')
-    sys.addNode('f', {alone:true, mass:.25})
+//    sys.addEdge('a','b')
+//    sys.addEdge('a','c')
+//    sys.addEdge('a','d')
+//    sys.addEdge('a','e')
+//    sys.addNode('f', {alone:true, mass:.25})
 
     // or, equivalently:
     //
