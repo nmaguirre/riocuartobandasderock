a1bb1c032ab0a6b6945e93ac0aac51a5db68cdb2
Ezequiel Depetris <ezedepetris@gmail.com>
2016-11-30 17:53:46 -0300

    add functionality to button to navigate on slide correctly

--- src/main/resources/webapp/assets/js/landing_page/landing_page.js
+++ src/main/resources/webapp/assets/js/landing_page/landing_page.js
@@ -1,5 +1,37 @@
 $(document).ready(function() {
   if($('#mycarousel').length){
     $("#mycarousel").carousel(1);
+
+
+    $('#mycarousel').on("click",'.slide-0', function(event){
+      event.preventDefault();
+      $("#mycarousel").carousel(0);
+    })
+
+    $('#mycarousel').on("click",'.slide-1', function(event){
+      event.preventDefault();
+      $("#mycarousel").carousel(1);
+    })
+
+    $('#mycarousel').on("click",'.slide-2', function(event){
+      event.preventDefault();
+      $("#mycarousel").carousel(2);
+    })
+
+    $('#mycarousel').on("click",'.slide-3', function(event){
+      event.preventDefault();
+      $("#mycarousel").carousel(3);
+    })
+
+    $('#mycarousel').on("click",'.slide-4', function(event){
+      event.preventDefault();
+      $("#mycarousel").carousel(4);
+    })
+    
+    $('#mycarousel').on("click",'.slide-5', function(event){
+      event.preventDefault();
+      $("#mycarousel").carousel(5);
+    })
+
   }
 });
