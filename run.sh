124d54da8cb0ad5ccd33cfd45082d29423104f0e
lbuttignol <leandrobuttignol@gmail.com>
2016-11-30 18:00:19 -0300

    add some BandMembers to test

--- run.sh
+++ run.sh
@@ -20,6 +20,7 @@ psql -h $new_host -p $new_port -U rock_db_owner -d rcrockbands -c "\COPY BandDB 
 psql -h $new_host -p $new_port -U rock_db_owner -d rcrockbands -c "\COPY AlbumDB FROM src/main/resources/database/Album.csv DELIMITERS ',' CSV ;"
 psql -h $new_host -p $new_port -U rock_db_owner -d rcrockbands -c "\COPY SongDB FROM src/main/resources/database/Song.csv DELIMITERS ',' CSV ;"
 psql -h $new_host -p $new_port -U rock_db_owner -d rcrockbands -c "\COPY artistDB FROM src/main/resources/database/Artist.csv DELIMITERS ',' CSV ;"
+psql -h $new_host -p $new_port -U rock_db_owner -d rcrockbands -c "\COPY BandMemberDB FROM src/main/resources/database/BandMember.csv DELIMITERS ',' CSV ;"
 echo "============ STARTING MAIN APP ============"
 mvn exec:java -Dexec.mainClass=ar.edu.unrc.exa.dc.dose2016.riocuartobandasderock.main.Bootstrap -Dexec.args="-dbPort $new_port -dbHost $new_host"
 echo "============= STOPING DOCKER  ============="
