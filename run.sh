a1d3cb9a3ede17030a2576ace4b6f0b966996dcd
lbuttignol <leandrobuttignol@gmail.com>
2016-11-30 05:41:42 -0300

    change echo

--- run.sh
+++ run.sh
@@ -7,13 +7,13 @@ mvn compile
 echo "============= STARTING DOCKER ============="
 docker run -d -p $new_port:5432 rcrockbands_db_container:latest > .docker_saved_pid
 sleep  5
-echo "============= STARTING DOCKER ============="
+echo "================ MAKE OWNER ==============="
 psql -h localhost -p $new_port:5432  -U postgres -d rcrockbands 
 \dt
 
 ## THIS COMMANDS SHOULD BE TYPED ON TERMINAL
-#grant postgres to rock_db_owner;
-#\q
+#	grant postgres to rock_db_owner;
+#	\q
 
 echo "========= INSERT INFO IN DATABASE ========="
 psql -h $new_host -p $new_port -U rock_db_owner -d rcrockbands -c "\COPY BandDB FROM src/main/resources/database/Band.csv DELIMITERS ',' CSV ;"
