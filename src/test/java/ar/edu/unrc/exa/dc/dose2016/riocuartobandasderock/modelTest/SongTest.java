f96c7c761cf38fdbc3bd6b23b8b86ebc1229057c
jortolano <ortolano33@gmail.com>
2016-12-01 21:43:28 -0300

    documentation added of the songDaoTest and songTest class

--- src/test/java/ar/edu/unrc/exa/dc/dose2016/riocuartobandasderock/modelTest/SongTest.java
+++ src/test/java/ar/edu/unrc/exa/dc/dose2016/riocuartobandasderock/modelTest/SongTest.java
@@ -6,9 +6,16 @@ import org.junit.Test;
 import ar.edu.unrc.exa.dc.dose2016.riocuartobandasderock.model.Album;
 import ar.edu.unrc.exa.dc.dose2016.riocuartobandasderock.model.Band;
 import ar.edu.unrc.exa.dc.dose2016.riocuartobandasderock.model.Song;
-
+/**
+ * 
+ * In this class are hosted the test corresponding to the class Song
+ */
 public class SongTest {
-
+            
+            /**
+             * test nameTest
+             * Description: This test confirms that the setName method define the name waited for a song
+             */
 	@Test
 	public void nameTest(){
 		Song song = new Song();
@@ -16,6 +23,10 @@ public class SongTest {
 		assertEquals("thebeatles", song.getName());
 	}
 	
+        /**
+         * test durationTest
+         * Description: Thist test confirms that the method setDuration define the duration waited for a song
+         */
 	@Test
 	public void durationTest(){
 		Song song = new Song();
@@ -23,6 +34,10 @@ public class SongTest {
 		assertEquals(3, song.getDuration());
 	}
 	
+        /**
+         * test constructorTest
+         * Description: This test confirms that the constructor of the class Song will generate a song expected
+         */
 	@Test
 	public void constructorTest(){
 		Song song = new Song("pinguinos en la cama", 300);
@@ -30,6 +45,10 @@ public class SongTest {
 		assertTrue(res);
 	}
 	
+        /**
+         * test getDurationInStringTest
+         * Description: This test confirms that the method getDurationAsString generated the string corresponding to the duration of the song
+         */
 	@Test
 	public void getDurationInStringTest(){
 		Song song = new Song();
@@ -38,6 +57,10 @@ public class SongTest {
 		assertEquals("2:9",song.getDurationAsString());
 	}
 	
+        /**
+         * test albumTest
+         * Description: This test confirms that the method setAlbum generated the album corresponding in the album field of song
+         */
  	@Test
  	public void albumTest(){
  		Album alb = new Album();
