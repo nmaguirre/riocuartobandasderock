bb9c07c86f19a4817311298f55bbeea572958ef0
MauriDV <mdellevedove12@gmail.com>
2016-12-02 17:08:30 -0300

    add toJson() method to band and song

--- src/main/java/ar/edu/unrc/exa/dc/dose2016/riocuartobandasderock/main/JsonUtil.java
+++ src/main/java/ar/edu/unrc/exa/dc/dose2016/riocuartobandasderock/main/JsonUtil.java
@@ -3,6 +3,8 @@ import java.lang.reflect.Type;
 import java.util.List;
 
 import ar.edu.unrc.exa.dc.dose2016.riocuartobandasderock.model.Album;
+import ar.edu.unrc.exa.dc.dose2016.riocuartobandasderock.model.Band;
+import ar.edu.unrc.exa.dc.dose2016.riocuartobandasderock.model.Song;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -11,9 +13,18 @@ import com.google.gson.reflect.TypeToken;
 import spark.*;
 
 public class JsonUtil {
+	public static String toJsonBand(Object object) {
+		Gson g = new GsonBuilder().setExclusionStrategies( new ExcludeFields() ).create();
+		Type type = new TypeToken<List<Band>>(){}.getType();
+		return g.toJson(object,type);
+	}
+	
+	public static ResponseTransformer jsonBand() {
+	    return JsonUtil::toJsonBand;
+	  }
+	
 	public static String toJson(Object object) {
 		return new Gson().toJson(object);
-	
 	}
 	
 	public static ResponseTransformer json() {
