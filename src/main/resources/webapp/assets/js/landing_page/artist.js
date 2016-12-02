e8fd29e31235d023a70ae57571814b9b3e178cc4
Ezequiel Depetris <ezedepetris@gmail.com>
2016-12-02 12:21:39 -0300

    add js yo view spotify

--- src/main/resources/webapp/assets/js/landing_page/artist.js
+++ src/main/resources/webapp/assets/js/landing_page/artist.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
   $('#mycarousel').on("click",'.artists-item', function(event){
+    console.log("ARTISTS-ITEM")
     event.preventDefault();
     $.ajax({
       'url': '/landing/artist/show',
@@ -8,12 +9,28 @@ $(document).ready(function() {
         "id": this.id
       }
     }).done(function(data) {  
+      console.log("Volvi de buscar banda")
       $('#artist-name').empty();
-      $('#artist-surname').empty();
       $('#artist-nickname').empty();
-      $('#artist-name').text(data.name);
-      $('#artist-surname').text(data.surname);
+      $('#artist-band').empty();
+
+      $('#artist-name').text(data.name+" "+data.surname);
       $('#artist-nickname').text(data.nickname);
+
+      for (var i = 0; i < data.bands.length; i++) {
+        var band = "<div class='track'>"
+       band += "<div class='track__number'>"+(i+1)+"</div>"
+       band += "<div class='track__added'>"
+       band += "<i class='ion-checkmark-round added'></i></div>"
+       band += "<div class='track__title'>"+data.bands[i].name+"</div>"
+       band += "<div class='track__explicit'>"+data.bands[i].genre+"</div>"
+       band += "<div class='track__popularity'>"
+       band += "<i class='ion-arrow-graph-up-right'></i>"
+       band += "</div></div>"
+
+        $('#artist-band').append(band);
+      }
+
       $("#mycarousel").carousel(3);
     });
 
